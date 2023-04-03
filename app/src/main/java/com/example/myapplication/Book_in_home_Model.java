2cc584270e9e291d87f172601deef53b5e474d0a
ducanh1811 <119037853+ducanh1811@users.noreply.github.com>
2023-04-03 22:53:45 +0700

    ducanh 01

---
+++ app/src/main/java/com/example/myapplication/Book_in_home_Model.java
@@ -0,0 +1,27 @@
+package com.example.uihome;
+
+public class Book_in_home_Model {
+    public String title;
+    public String url;
+
+    public Book_in_home_Model(String title, String url) {
+        this.title = title;
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}

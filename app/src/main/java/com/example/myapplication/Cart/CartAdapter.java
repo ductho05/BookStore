6904cee798542c1b289fc66be7e43acb13a0d58d
Thim7 <20110653@student.hcmute.edu.vn>
2023-04-04 00:24:03 +0700

    Update Cart

---
+++ app/src/main/java/com/example/myapplication/Cart/CartAdapter.java
@@ -0,0 +1,66 @@
+package com.example.myapplication.Cart;
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.myapplication.R;
+
+import java.util.ArrayList;
+
+public class CartAdapter extends RecyclerView.Adapter<CartAdapter.ViewHolder>{
+    ArrayList<CartModel> listDomains;
+
+    public CartAdapter(ArrayList<CartModel> listDomains) {
+        this.listDomains = listDomains;
+    }
+
+
+    @NonNull
+    @Override
+    public CartAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_cart, parent, false);
+        return new ViewHolder(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CartAdapter.ViewHolder holder, int position) {
+        holder.nameItem.setText(String.valueOf(listDomains.get(position).getNameItem()));
+        holder.priceItem.setText(String.valueOf(listDomains.get(position).getPriceItem()));
+        holder.salePriceItem.setText(String.valueOf(listDomains.get(position).getSalePriceItem()));
+        holder.amountItem.setText(String.valueOf(listDomains.get(position).getAmount()));
+
+
+
+        int drawableResourceId = holder.itemView.getContext().getResources().getIdentifier(listDomains.get(position).getImageItem(),
+                "drawable", holder.itemView.getContext().getPackageName());
+        Glide.with(holder.itemView.getContext())
+                .load(drawableResourceId)
+                .into(holder.imageItem);
+    }
+
+    @Override
+    public int getItemCount() {
+        return listDomains.size();
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView nameItem, priceItem, salePriceItem, amountItem;
+        ImageView imageItem;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            nameItem = itemView.findViewById(R.id.nameItem);
+            priceItem = itemView.findViewById(R.id.priceItem);
+            salePriceItem = itemView.findViewById(R.id.salePriceItem);
+            amountItem = itemView.findViewById(R.id.amountItem);
+            imageItem = itemView.findViewById(R.id.imageItem);
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/myapplication/Cart/CartModel.java
@@ -0,0 +1,57 @@
+package com.example.myapplication.Cart;
+
+public class CartModel {
+    private String nameItem;
+    private String priceItem;
+    private String salePriceItem;
+    private String imageItem;
+    private int amount;
+
+    public CartModel(String nameItem, String priceItem, String salePriceItem, String imageItem, int amount) {
+        this.nameItem = nameItem;
+        this.priceItem = priceItem;
+        this.salePriceItem = salePriceItem;
+        this.imageItem = imageItem;
+        this.amount = amount;
+    }
+
+    public String getNameItem() {
+        return nameItem;
+    }
+
+    public String getPriceItem() {
+        return priceItem;
+    }
+
+    public String getSalePriceItem() {
+        return salePriceItem;
+    }
+
+    public String getImageItem() {
+        return imageItem;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setNameItem(String nameItem) {
+        this.nameItem = nameItem;
+    }
+
+    public void setPriceItem(String priceItem) {
+        this.priceItem = priceItem;
+    }
+
+    public void setSalePriceItem(String salePriceItem) {
+        this.salePriceItem = salePriceItem;
+    }
+
+    public void setImageItem(String imageItem) {
+        this.imageItem = imageItem;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}

c2247156455daf0219ac332ce60a3e3311a0ab4f
ductho05 <ductho23pro@gmail.com>
2023-04-05 23:59:24 +0700

    update cart

--- app/src/main/java/com/example/myapplication/adapter/CartAdapter.java
+++ app/src/main/java/com/example/myapplication/adapter/CartAdapter.java
@@ -1,4 +1,4 @@
-package com.example.myapplication.Cart;
+package com.example.myapplication.adapter;
 
 
 import android.view.LayoutInflater;
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.myapplication.R;
+import com.example.myapplication.model.CartModel;
 
 import java.util.ArrayList;
 
--- app/src/main/java/com/example/myapplication/model/CartModel.java
+++ app/src/main/java/com/example/myapplication/model/CartModel.java
@@ -1,4 +1,4 @@
-package com.example.myapplication.Cart;
+package com.example.myapplication.model;
 
 public class CartModel {
     private String nameItem;
